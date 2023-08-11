4c5a5b04392666ac2cd1640ca92053b1d1bc2830
marc ambrocio <mikedbambrocio@gmail.com>
2023-08-11 09:03:05 -0400

    Started fetch on geolocation

--- scripts/apiKey.js
+++ scripts/apiKey.js
@@ -1,7 +1,5 @@
 
-const api = {
-	key: "897d9a2cdae4ca6db4e43c2cde07d6c7",
-	base: "https://api.openweathermap.org/data/2.5/"
-}
-
-export default api;
+// const api = {
+// 	key: "897d9a2cdae4ca6db4e43c2cde07d6c7",
+// 	base: "https://api.openweathermap.org/data/2.5/"
+// }
--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,51 @@
-document.getElementById("bg").volume = 0.38;
+let bgMusic = document.getElementById("bg");
+bgMusic.volume = 0.22;
 
+let weatherSearch = document.getElementById('weatherSearch');
+
+// let city = document.getElementById('city').value;
+// const inputEnter = document.getElementById('weatherSearch');
+// inputEnter.addEventListener("keyPress", function(event) {
+
+//     if(event.key === "Enter"){
+//         event.preventDefault();
+//         weatherSearch.addEventListener('click', cityDisplay);
+//     }
+// });
+
+function cityDisplay() {
+    let city = document.getElementById('city').value;
+    let displaySearch = document.getElementById('cityDisplay').innerHTML = city;
+    console.log(displaySearch);
+
+    return false;
+}
+cityDisplay();
+
+function fetchWeather(){
+    let city = document.getElementById('city').value;
+
+
+
+    let locName = `http://api.openweathermap.org/geo/1.0/direct?q=` + city + `&appid=238eea4f33d97368ff0040161d3e7379`;
+
+    console.log(locName);
+
+        fetch(locName)
+            .then(function (response){
+                return response.json();
+            })
+            
+            .then(function (data){
+                console.log(data);
+                let cityLat = data[0].lat;
+                let cityLon = data[0].lon;
+    
+                console.log(cityLat);
+                console.log(cityLon);
+            })
+        fetc
+}
+
+weatherSearch.addEventListener('click', cityDisplay);   
+weatherSearch.addEventListener('click', fetchWeather)
